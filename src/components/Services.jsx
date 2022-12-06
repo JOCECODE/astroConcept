72de740bdcbddc7539e753dce74cf6ef010a029c
JOCECODE <ulah57@gmail.com>
2022-12-06 03:55:00 -0800

    firstComponent;

---
+++ src/components/Services.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <div className='blue'><p className='blushText text-9xl font-bold text-center'>Services</p> </div>
+  )
+}
+
+export default Services;
