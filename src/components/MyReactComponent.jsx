04cf7e683f098edb83833120995f6bbf803efe45
JOCECODE <ulah57@gmail.com>
2022-12-06 00:34:45 -0800

    setup

---
+++ src/components/MyReactComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MyReactComponent() {
+  return (
+    <div className='text-blue-600 checker'>MyReactComponent</div>
+  )
+}
+
+export default MyReactComponent;
